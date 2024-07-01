f8cd129e5ed13f55f0e1d40b4b13f0a96bb628e6
Vladislav Fatov <vladfatov1054@gmail.com>
2024-07-01 14:44:28 +0200

    Added system commentation

--- appblog/models.py
+++ appblog/models.py
@@ -10,4 +10,16 @@ class Post(models.Model):
 
 
     def __str__(self):
-        return self.title
+        return self.title    
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
--- appblog/views.py
+++ appblog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from . import models
+from .forms import CommentForm
 
 
 def index(request):
@@ -16,9 +17,23 @@ def show_posts(request):
 
 
 def detail_post(request, post_id):
-    post = models.Post.objects.get(pk=post_id)
+    post = get_object_or_404(models.Post, pk=post_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+
+            
+            return redirect('detail', post_id=post.pk)
+    else:
+        form = CommentForm()
 
     context = {'title': 'Detail Post',
                'post': post,
+               'form': form,
                }
     return render(request, 'appblog/detail_post.html', context=context)
